9ee387be24ec3fa1a711ad14b3a9f14068aa49f1
Rachael Mohl <rmohl@az-whisper-p.pi.local>
2023-06-07 10:34:04 -0400

    implemented model class interface

--- models/WhisperPI.py
+++ models/WhisperPI.py
@@ -1,18 +1,23 @@
 from os.path import basename
 from time import time
 from datetime import timedelta
+from ModelFormat import ModelFormat
 import whisper
 
-class WhisperPI:
+class WhisperPI(ModelFormat):
+
+    name = ""
+    model_type = ""
+    options = {}
+    full_result = {}
+    transcription = {}
+    load_time = {}
+    transcribe_time = {}
 
     def __init__(self, name, options):
         self.name = name
         self.model_type = options.pop("model_type", "large")
         self.options = options
-        self.result_object = {}
-        self.transcription = {}
-        self.load_time = {}
-        self.transcribe_time = {}
 
     def transcribe(self, audio, prompt=None):
 
@@ -28,10 +33,10 @@ class WhisperPI:
         result = model.transcribe(audio, initial_prompt=prompt, **self.options)
         transcribe_end = time()
         
-        # save load and transcribe times
+        # save load time, and transcribe time, and full result
         self.load_time.update({audio_name: str(timedelta(seconds=load_end - load_start))})
         self.transcribe_time.update({audio_name: str(timedelta(seconds=transcribe_end - transcribe_start))})
-        self.result_object.update({audio_name: result})
+        self.full_result.update({audio_name: result})
 
         # save transcription
         transcription = ""
