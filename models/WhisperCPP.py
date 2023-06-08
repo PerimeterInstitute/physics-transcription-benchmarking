ae20d4b853ed4711657b349d82321370ea10aa49
Rachael Mohl <rmohl@az-whisper-p.pi.local>
2023-06-08 17:01:59 -0400

    model is loaded in init(), WhisperCPP model now works with prompts

--- models/WhisperCPP.py
+++ models/WhisperCPP.py
@@ -4,20 +4,31 @@ from time import time
 from datetime import timedelta
 from ModelFormat import ModelFormat
 from whispercpp import Whisper
+from whisper.tokenizer import get_tokenizer
 
 class WhisperCPP(ModelFormat):
 
     name = ""
     model_type = ""
     options = {}
+    model = None
+
     transcription = {}
     load_time = {}
     transcribe_time = {}
+    
 
     def __init__(self, name, options):
         self.name = name
         self.model_type = options.pop("model_type", "large")
         self.options = options
+
+        # download/load model
+        load_start = time()
+        self.model = Whisper.from_pretrained(self.model_type)
+        load_end = time()
+
+        self.__load_time__ = load_end - load_start
 
     def transcribe(self, audio, prompt=None):
 
@@ -25,32 +36,31 @@ class WhisperCPP(ModelFormat):
         audio_wav = audio.rsplit(".", 1)[0] + ".wav"
         audio_ext = audio.rsplit(".", 1)[1]
 
-        # converting audio file to .wav file (only if .wav file has not already been created and current audio file is not .wav file)
+        # converting audio file to .wav file (only if .wav file has not already been created AND current audio file is not .wav file)
         if exists(audio_wav):
             audio = audio_wav
         elif not audio_ext == "wav":
             system("ffmpeg -i " + audio + " -ar 16000 -ac 1 -c:a pcm_s16le " + audio_wav)
             audio = audio_wav
 
-        # download/load model
-        load_start = time()
-        model = Whisper.from_pretrained(self.model_type)
-        load_end = time()
-
-        # set prompt --> find a way to convert prompt to tokens? (maybe just copy from whisper code)
-        # if not prompt == None:
-        #     model.params.set_tokens(prompt.split(" "))
+        # set prompt
+        if not prompt == None:
+            # tokenizing prompt
+            tokenizer = get_tokenizer(multilingual=False, language=self.options["language"], task="transcribe")
+            prompt_tokens = tokenizer.encode(" " + prompt.strip())
+            # setting prompt tokens
+            self.model.params.set_tokens(prompt_tokens)
 
         # set options
         for key in self.options:
-            exec('model.params.with_'+key+'(self.options["'+key+'"])')      # using exec() function so param setter function can change
+            exec('self.model.params.with_'+key+'(self.options["'+key+'"])')      # using exec() function so param setter function can change
 
         # transcribe audio
         transcribe_start = time()
-        transcription = model.transcribe_from_file(audio)
+        transcription = self.model.transcribe_from_file(audio)
         transcribe_end = time()
         
         # save load time, transcribe time, and transcription
-        self.load_time.update({audio_name: str(timedelta(seconds=load_end - load_start))})
+        self.load_time.update({audio_name: str(timedelta(seconds=self.__load_time__))})
         self.transcribe_time.update({audio_name: str(timedelta(seconds=transcribe_end - transcribe_start))})
         self.transcription.update({audio_name: transcription})
--- models/WhisperPI.py
+++ models/WhisperPI.py
@@ -9,6 +9,8 @@ class WhisperPI(ModelFormat):
     name = ""
     model_type = ""
     options = {}
+    model = None
+
     full_result = {}
     transcription = {}
     load_time = {}
@@ -19,22 +21,24 @@ class WhisperPI(ModelFormat):
         self.model_type = options.pop("model_type", "large")
         self.options = options
 
+        # load model
+        load_start = time()
+        self.model = whisper.load_model(self.model_type)
+        load_end = time()
+
+        self.__load_time__ = load_end - load_start
+
     def transcribe(self, audio, prompt=None):
 
         audio_name = basename(audio)
 
-        # load model
-        load_start = time()
-        model = whisper.load_model(self.model_type)
-        load_end = time()
-
         # transcribe audio
         transcribe_start = time()
-        result = model.transcribe(audio, initial_prompt=prompt, **self.options)
+        result = self.model.transcribe(audio, initial_prompt=prompt, **self.options)
         transcribe_end = time()
         
         # save load time, and transcribe time, and full result
-        self.load_time.update({audio_name: str(timedelta(seconds=load_end - load_start))})
+        self.load_time.update({audio_name: str(timedelta(seconds=self.__load_time__))})
         self.transcribe_time.update({audio_name: str(timedelta(seconds=transcribe_end - transcribe_start))})
         self.full_result.update({audio_name: result})
 
