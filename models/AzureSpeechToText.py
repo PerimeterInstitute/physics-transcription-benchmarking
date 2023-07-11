788b6af379e5348fd08339396f55b86bf3d5895f
rmohl <mohlrachael@gmail.com>
2023-07-11 13:51:01 -0400

    finalized wrapper for Azure Speech to Text

--- models/AzureSpeechToText.py
+++ models/AzureSpeechToText.py
@@ -9,7 +9,10 @@ class AzureSpeechToText(ModelWrapper):
     __speech_config = None
 
     name = ""
+    key = ""
+    region = ""
     options = {}
+    takes_prompt = False
 
     transcription = {}
     load_time = {}
@@ -17,14 +20,26 @@ class AzureSpeechToText(ModelWrapper):
 
     def __init__(self, name, key, region, options):
         self.name = name
+        self.key = key
+        self.region = region
         self.options = options
+
+    def load(self):
 
         # create speech configuration
         load_start = time()
-        self.__speech_config = speechsdk.SpeechConfig(subscription=key, region=region, speech_recognition_language=self.options.pop("language", None))
+        self.__speech_config = speechsdk.SpeechConfig(subscription=self.key, region=self.region, speech_recognition_language=self.options.pop("language", None))
         load_end = time()
 
         self.__load_time__ = load_end - load_start
+
+    def unload(self):
+        del self.__speech_config
+        del self.name
+        del self.key
+        del self.region
+        del self.options
+        del self.takes_prompt
 
     def transcribe(self, audio, prompt=None):
 
@@ -36,18 +51,27 @@ class AzureSpeechToText(ModelWrapper):
         audio_config = speechsdk.audio.AudioConfig(filename=audio)
         speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.__speech_config, audio_config=audio_config)
         
-        # TODO: this will only transcribe max 15s, do something with start_continuous_recognition_async() func instead
-        result = speech_recognizer.recognize_once()     
+        result = ""
+        finished = False
+        while not finished:
 
-        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-            print("Recognized by Azure: {}".format(result.text))
-        elif result.reason == speechsdk.ResultReason.NoMatch:
-            print("No speech could be recognized: {}".format(result.no_match_details))
-        elif result.reason == speechsdk.ResultReason.Canceled:
-            cancellation_details = result.cancellation_details
-            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-            if cancellation_details.reason == speechsdk.CancellationReason.Error:
-                print("Error details: {}".format(cancellation_details.error_details))
+            utterance = speech_recognizer.recognize_once()     
+
+            if utterance.reason == speechsdk.ResultReason.RecognizedSpeech:
+                result += utterance.text + "\n"
+
+            elif utterance.reason == speechsdk.ResultReason.NoMatch:
+                if utterance.no_match_details.reason == speechsdk.NoMatchReason.InitialSilenceTimeout:      # if the end of the audio file was reached
+                    finished = True
+                else:
+                    print("No speech could be recognized: {}".format(utterance.no_match_details))
+
+            elif utterance.reason == speechsdk.ResultReason.Canceled:
+                cancellation_details = utterance.cancellation_details
+                print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+                if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                    print("Error details: {}".format(cancellation_details.error_details))
+                finished = True
 
         transcribe_end = time()
         
