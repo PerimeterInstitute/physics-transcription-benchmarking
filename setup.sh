bfacf98bf53f6971abc9ed25c09e0e7b05b16535
rmohl <mohlrachael@gmail.com>
2024-06-07 12:49:46 -0400

    first draft of setup.sh file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Install dependencies:
+
+pip install jiwer
+pip install -U openai-whisper
+
+# Download Full and Dev Dataset WAV Files :p
+
+# check if wget is available (?)
+
+wget http://example.com/file.tar -O ./datasets/full_dataset/test_data
